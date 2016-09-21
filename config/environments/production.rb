293fba87f4cab85ed04203f449b203d7aad1ca03
Yrysbekov Aibek <yrysbekov.a@gmail.com>
2016-09-21 12:48:13 +0600

    #2 added gem mailcatcher and configured devise to send a confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+      :address => "127.0.0.1",
+      :port    => 25,
+      :domain  => 'yourdomain.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,13 @@ ActiveRecord::Schema.define(version: 20160921045055) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
