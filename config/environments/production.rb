380b2396cff49c884f657651589b74ca45c67e81
Yrysbekov Aibek <yrysbekov.a@gmail.com>
2016-09-21 13:18:33 +0600

    #2 added gem mailcatcher and configured devise to send a confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-      :address => "127.0.0.1",
-      :port    => 25,
-      :domain  => 'yourdomain.com'}
+  config.action_mailer.default_url_options = {:host => 'gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'kgimho@gmail.com',
+      :password             => 'ESDP@2016',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
