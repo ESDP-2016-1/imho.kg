d9c2a9492f2f6ab4826c9adfe2883adb93ce0a85
Arseniy <arseniy.tarapon@gmail.com>
2016-09-23 14:16:02 +0600

    #3 added center form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
                     styles: { medium: '300x300>', thumb: '100x100>'},
-                    default_url: ':style/missing.png'
-  validates_attachment_content_type :avatar,
-                                    content_type: ['image/jpeg', 'image/gif', 'image/png']
-
+                    default_url: ':images/:style/missing.png'
+  validates_attachment :avatar,
+                       content_type: { content_type: %w( image/jpeg image/jpg image/gif image/png) }
 end
