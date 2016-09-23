1945d89a2aabadb811b82a5c0d7a0d2c3d6f7260
Yrysbekov Aibek <yrysbekov.a@gmail.com>
2016-09-23 13:59:10 +0600

    #3 added and modified gem paperclip for users to upload avatars, and users now can edit their pages. Added date of birth and gender fields for users. Added missing png files if user doesnt upload an avatar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, presence: true, length: {maximum: 50}
+
   has_attached_file :avatar,
                     styles: { medium: '300x300>', thumb: '100x100>'},
                     default_url: ':style/missing.png'
