5f4cebc1158ec39447661edab0c99f8a43cdd11e
Vasily Toropchin <vasview@gmail.com>
2016-10-19 14:03:59 +0600

    #39 Added translation to edit form of user.
    Добавил файл ru.yml в config/locale, данный файл используется для локализации перевода форм.
    Создал паршиал для формы редактирования. Поменял выбор пола на радио-кнопки.
    
    Необходимо, доработать обработку ошибок. Сделать их на стороне клиента.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,28 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = "You've updated your profile."
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong, please, try again."
+      render 'edit'
+    end 
+
   end
 
   def destroy
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:fullname, :name, :email, :password, :password_confirmation, :dob, :avatar, :gender_id, :city_id, :role_id )
   end
 end
