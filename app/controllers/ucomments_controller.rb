04a85e5e8ec55318f356a634617b5b80813ae37b
Yrysbekov Aibek <yrysbekov.a@gmail.com>
2016-10-27 02:16:57 +0600

    # 41 CRUD for Ucomments

--- app/controllers/ucomments_controller.rb
+++ app/controllers/ucomments_controller.rb
@@ -3,11 +3,27 @@ class UcommentsController < ApplicationController
   end
 
   def create
+    @comment = Ucomment.new(comment_params)
+    if @comment.save
+      redirect_to  :back
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @comment = Ucomment.new
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:ucomment).permit(:title, :body, :positive, :rate, :user_id, :company_id)
   end
 end
--- app/models/ucomment.rb
+++ app/models/ucomment.rb
@@ -1,5 +1,4 @@
 class Ucomment < ActiveRecord::Base
-  #acts_as_votable
   belongs_to :user
   belongs_to :company
   has_many :subucomments, class_name: "Ucomment", foreign_key: 'topucomment_id'
