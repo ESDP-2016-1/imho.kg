e1a90fe84bbfdd95ad3ed35cc3adf47617b60ae4
Vasily Toropchin <vasview@gmail.com>
2016-11-17 15:32:32 +0600

    #41 Add ability to delete comment

--- app/controllers/ucomments_controller.rb
+++ app/controllers/ucomments_controller.rb
@@ -41,9 +41,35 @@ class UcommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Ucomment.find(params[:ucomment])
+    if @comment.topucomment_id.nil?
+      while @comment.subucomments.present?
+        for subcomment in @comment.subucomments do
+          destroy_topucomment_and_its_childs(subcomment)
+        end  
+      end
+      @comment.destroy
+      redirect_to root_path
+    else
+      
+      @comment.body = "Комментарий был удален"
+      @comment.save
+      binding.pry
+      redirect_to :back
+    end  
   end
 
   private
+
+  def destroy_topucomment_and_its_childs(comment)
+      if comment.subucomments.present?
+        comment.subucomments.each do |subucomment|
+          destroy_topucomment_and_its_childs(subucomment)
+        end  
+      else
+        comment.destroy
+      end  
+  end  
 
   def comment_params
     params.require(:ucomment).permit(:title, :body,
