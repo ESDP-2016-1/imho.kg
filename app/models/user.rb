9730943cce0e5c194c81196617d8b5054326c6ce
Dmitrii <zagvozkin.dmitry@gmail.com>
2016-11-25 00:16:37 +0600

    #53 Рефакторинг Шаг: 1
    Исправления:
    - Создание хелперов helper_draw_company_card и helper_draw_comment_card
    - Исправление изменений профиля

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  #before_update :check_fileds, :if => path
+
   #acts_as_voter
   belongs_to :gender
   belongs_to :city
@@ -7,19 +9,15 @@ class User < ActiveRecord::Base
   has_many :ucomments
   has_many :favorites
   has_many :votes
-  # before_create :set_default_role
-  # or
-  # before_validation :set_default_role
 
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         
-  validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+
+  validates :name, presence: true, uniqueness: true, length: {minimum:3, maximum: 50}
 
   validates :email, presence: true, uniqueness: true
+
+  #validates :password, presence: true, length: {minimum: 6, maximum: 250}
 
   has_attached_file :avatar,
                     styles: { medium: '250x250>', thumb: '80x80', icon: '50x50'},
@@ -28,11 +26,24 @@ class User < ActiveRecord::Base
   validates_attachment :avatar,
                        content_type: { content_type: %w( image/jpeg image/jpg image/gif image/png) }
 
+  def apply_updates(parameters, active_user, received_password)
 
-  private
+    if active_user.valid_password?(received_password)
 
-  # def set_default_role
-  #   self.role ||= Role.find_by(name: 'user')
-  # end
+    self.assign_attributes(parameters)
+
+      if self.changed?
+        if self.update_attributes!(parameters)
+          return true
+        end
+      end
+
+    end
+
+  end
+
+  protected
+
+
 
 end
