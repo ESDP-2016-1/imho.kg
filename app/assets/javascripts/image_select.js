ee64680ca37f5ae8974d07aacd6c9bda2045f341
Dmitrii <zagvozkin.dmitry@gmail.com>
2016-12-09 13:56:44 +0600

    #63 дополнения

--- app/assets/javascripts/image_select.js
+++ app/assets/javascripts/image_select.js
@@ -1,9 +1,11 @@
 close_button_image = "/images/close-image-btn.png";
 preffix_of_hidden_fields = '#image_select_';
+default_image_path = '';
 
 function AddImageBlock(div_block, amount, default_image){
     div_block = div_block.selector ? div_block : $(div_block);
     div_block.empty();
+    default_image_path = default_image;
     for(var i=1; i<=amount; i++) {
         div_block.append(DrawSelectImage(default_image, i));
         div_block.find('.class-close-image').hide();
@@ -42,7 +44,10 @@ $(document).ready( function() {
 
     $(document).on('click', '.class-close-image', function(){
         var id=$(this).attr('data-id');
-
+        var image_field = $(this).siblings('.class-image');
+        $(this).hide();
+        image_field.attr('src', default_image_path);
+        $("#hidden_form > input[data-id='" + id + "']").val('');
     });
 
     $('input[type=file]').change(function(e){
